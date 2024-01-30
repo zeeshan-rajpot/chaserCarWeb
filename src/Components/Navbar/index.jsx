ee3e665f7cecedc1c48a54557d419a379e56bc7d
zeeshan-rajpot <zeeshanrajpot74@gmail.com>
2024-01-30 22:23:14 +0500

    new

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./LandingPage.css";
+// import "./LandingPage.css";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
